1468c5985f6e01dff214be6f240cecc44d007b86
frannythebanny <f.burger91@gmail.com>
2014-11-28 16:05:34 +0100

    merge everything

---
+++ PhotoAlbum/src/userInterface/commandInterface.java
@@ -0,0 +1,19 @@
+package userInterface;
+
+public interface commandInterface {
+
+	abstract void selectPicture(int nr); //Voice
+	abstract void nextPage();//Voice and/or LEAP
+	abstract void previousPage(); //Voice and/or LEAP
+	abstract void selectSpecificPage(int nr); //Voice
+	abstract void insertPages(); //Insert 2 page after current page. //Voice or LEAP?
+	abstract void removeCurrentPages(); //Voice or LEAP?
+	
+
+
+
+
+
+
+
+}
