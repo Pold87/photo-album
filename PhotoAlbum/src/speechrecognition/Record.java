2999a2e73950b91d8abdc7cefc462111764b28a0
frannythebanny <f.burger91@gmail.com>
2014-12-04 11:20:35 +0100

    some speech stuff

--- PhotoAlbum/src/speechrecognition/Record.java
+++ PhotoAlbum/src/speechrecognition/Record.java
@@ -1,11 +1,5 @@
 package speechrecognition;
 
-import it.sauronsoftware.jave.AudioAttributes;
-import it.sauronsoftware.jave.Encoder;
-import it.sauronsoftware.jave.EncoderException;
-import it.sauronsoftware.jave.EncodingAttributes;
-import it.sauronsoftware.jave.InputFormatException;
-
 import javax.sound.sampled.*;
 
 import java.io.*;
@@ -92,17 +86,17 @@ public class Record {
 		System.out.println("Finished");
 	}
 	
-	public void convertWavToMp3(File source, File target) throws Exception {
-		AudioAttributes audio = new AudioAttributes();
-		audio.setCodec("libmp3lame");
-		audio.setBitRate(new Integer(128000));
-		audio.setChannels(new Integer(2));
-		audio.setSamplingRate(new Integer(44100));
-		EncodingAttributes attrs = new EncodingAttributes();
-		attrs.setFormat("mp3");
-		attrs.setAudioAttributes(audio);
-		Encoder encoder = new Encoder();
-		encoder.encode(source, target, attrs);
-	}
+//	public void convertWavToMp3(File source, File target) throws Exception {
+//		AudioAttributes audio = new AudioAttributes();
+//		audio.setCodec("libmp3lame");
+//		audio.setBitRate(new Integer(128000));
+//		audio.setChannels(new Integer(2));
+//		audio.setSamplingRate(new Integer(44100));
+//		EncodingAttributes attrs = new EncodingAttributes();
+//		attrs.setFormat("mp3");
+//		attrs.setAudioAttributes(audio);
+//		Encoder encoder = new Encoder();
+//		encoder.encode(source, target, attrs);
+//	}
 
 }
